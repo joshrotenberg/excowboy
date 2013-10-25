2a27001df8c829df8f047b22ef0f00493b054e29
josh rotenberg <joshrotenberg@gmail.com>
2013-10-25 09:27:40 -0700

    add package.exs

---
+++ package.exs
@@ -0,0 +1,5 @@
+Expm.Package.new(name: "excowboy", description: "A Mix task to create a Cowboy project skeleton",
+                 version: "0.0.1", keywords: ["mix", "cowboy"],
+                 maintainers: [[name: "Josh Rotenberg",
+                                email: "joshrotenberg@gmail.com"]],
+                 repositories: [[github: "joshrotenberg/excowboy"]])
